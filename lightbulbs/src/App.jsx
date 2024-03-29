e6d1b59e52985f301c2b6d7ea3928d4cac39c964
TrevSh <trevonshelby@outlook.com>
2024-03-29 15:42:40 -0500

    created Lightbulb page

---
+++ lightbulbs/src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+import './App.css'
+import LIGHT from './light.jsx'
+
+const LIGHT_OFF = './src/assets/light_off.jpg'
+const LIGHT_ON = './src/assets/light_on.png'
+
+//SEPERATE EVERYTHING INTO SEPERATE COMPONENT CALLED LIGHT (CAPITAL)
+//Render 5 light components
+//MUST HAVE 5 LIGHT BULBS AND 5 SWITCHES EACH WITH THEIR OWN STATE
+
+//AFTER, CREATE NEW COMPONENT CALLED MASTER SWITCH
+//MASTER SWITCH WILL CONTROL ANOTHER PIECE OF STATE THAT CAN BE PASSED
+//INTO EACH LIGHT AS PROP
+//IF MASTER SWITCH IS ON, ALL SWITCHES ARE ON. ELSE, NORMAL FUNCTIONS
+
+  //A prop is a passable varibale
+  //WATCH YOUTUBE VIDEO IN EMAIL FROM TYLER
+
+export function MASTERSWITCH(){
+  const [isGlobalOn, setGlobalSwitch] = useState(false);
+
+  const toggleLights = () => {
+    setGlobalSwitch(!isGlobalOn)
+    console.log(isGlobalOn);
+  };
+
+  return (
+    <div>
+      <h1>These are Lightbulbs</h1>
+      <button onClick={toggleLights}>This is the MasterButton</button>
+      <div className='SwitchBox'>
+        <LIGHT globalSwitch = {isGlobalOn}/>
+        <LIGHT  globalSwitch = {isGlobalOn}/>
+        <LIGHT  globalSwitch = {isGlobalOn}/>
+        <LIGHT  globalSwitch = {isGlobalOn}/>
+        <LIGHT  globalSwitch = {isGlobalOn}/>
+      </div>
+    </div>
+  )
+}
+
+export default MASTERSWITCH;
---
+++ lightbulbs/src/light.jsx
@@ -0,0 +1,34 @@
+import { useState, useEffect } from 'react'
+
+function LIGHT({globalSwitch}) {
+    const LIGHT_OFF = './src/assets/light_off.jpg'
+    const LIGHT_ON = './src/assets/light_on.png'
+
+    const [imgPath, setImgPath] = useState(LIGHT_OFF)
+
+    //useEffect runs each time [globalSwitch] is changed.
+    useEffect(() => {
+            setImgPath(globalSwitch ? LIGHT_ON : LIGHT_OFF);
+    }, [globalSwitch]);
+
+    const toggleLight = () => {
+        console.log("Toggled")
+        if(!globalSwitch){
+            const newPath = imgPath === LIGHT_ON ? LIGHT_OFF : LIGHT_ON
+            setImgPath(newPath);
+    }
+}
+
+    return (
+        <div className="card">
+          <img src={imgPath} style={{height:'100px',width:'100px'}}/>
+          <div>
+            <button onClick={toggleLight}>
+               Toggle Light
+            </button>
+          </div>
+        </div>
+    )
+}
+
+export default LIGHT;
